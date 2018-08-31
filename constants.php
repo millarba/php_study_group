b18be285054643aa25839fb4f0e0528b6e1ccf37
Ben Millar <bmillar@MacBook-Pro-7.local>
2018-08-31 14:29:35 -0500

    use constants for the first time and use both single and double quotes for variable interpretation

---
+++ constants.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Constants!</title>
+</head>
+<body>
+
+<?php
+	# Script 1.9 - constants.php
+	define('TODAY', 'August 31, 2018');
+
+	echo '<p>Today is ' . TODAY . '<br />This server is running version <b>' . PHP_VERSION . '</b> of PHP on the <b>' . PHP_OS . '</b> operating system.</p>';
+?>
+
+</body>
+</html>
--- numbers.php
+++ numbers.php
@@ -15,8 +15,11 @@
 	$total = $total + ($total * $taxrate);
 	$total = number_format($total, 2);
 
-	echo '<p>You are purchasing <b>' . $quantity . '</b> widget(s) at a cost of <b>$' . $price . '</b> each. With tax, the total comes to <b>$' . $total . '</b>.</p>';
+	echo "<h3>Using double quotation marks:</h3>";
+	echo "<p>You are purchasing <b>$quantity</b> widget(s) at a cost of <b>\$$price</b> each. With tax, the total comes to <b>\$$total</b>.</p>\n";
 
+	echo '<h3>Using single quotation marks:</h3>';
+	echo '<p>You are purchasing <b>$quantity</b> widget(s) at a cost of <b>\$$price</b> each. With tax, the total comes to <b>\$$total</b>.</p>\n';
 ?>
 
 </body>
