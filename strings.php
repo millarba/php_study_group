ee5c2e4559cfb93752c5214ac095f986fade0dd5
Ben Millar <bmillar@MacBook-Pro-7.local>
2018-08-31 12:37:18 -0500

    concatenate two variables to create a new variable using first_name and last_name

--- strings.php
+++ strings.php
@@ -10,9 +10,10 @@
 
 	$first_name = "Haruki";
 	$last_name = "Murakami";
+	$author = $first_name . " " . $last_name;
 	$book = "Kafka on the Shore";
 
-	echo "<p>The book <em>$book</em> was written by $first_name $last_name.";
+	echo "<p>The book <em>$book</em> was written by $author.";
 ?>
 
 </body>
