2f0bf5c0c0520c2796ef8c0302e3b2df28571687
Ben Millar <bmillar@MacBook-Pro-7.local>
2018-08-31 12:34:39 -0500

    print basic string values

---
+++ strings.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Strings!</title>
+</head>
+<body>
+
+<?php
+	// Script 1.6 - strings.php
+
+	$first_name = "Haruki";
+	$last_name = "Murakami";
+	$book = "Kafka on the Shore";
+
+	echo "<p>The book <em>$book</em> was written by $first_name $last_name.";
+?>
+
+</body>
+</html>
