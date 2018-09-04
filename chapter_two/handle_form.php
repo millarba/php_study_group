1c0bab0c52ea09b5160697ceeac3ee784fca65a1
Ben Millar <bmillar@MacBook-Pro-7.local>
2018-09-03 19:02:49 -0500

    add conditonal for gender input value

--- chapter_two/handle_form.php
+++ chapter_two/handle_form.php
@@ -11,8 +11,24 @@
 	$name = $_REQUEST["name"];
 	$email = $_REQUEST["email"];
 	$comments = $_REQUEST["comments"];
+	$age = $_REQUEST["age"];
+
+	if (isset($_REQUEST["gender"])) {
+		$gender = $_REQUEST["gender"];
+	} else {
+		$gender = NULL;
+	}
 
 	echo "<p>Thank you, <b>$name</b>, for the following comments: <br /> <tt>$comments</tt></p><p>We will reply to you at <i>$email</i>.</p>\n";
+
+	if ($gender == "M") {
+		echo "<p><b>Good day, Sir!</b></p>";
+	} elseif ($gender == "F") {
+		echo "<p><b>Good day, Madam!</b><p>";
+	} else {
+		echo "<p><b>You forgot to enter your gender!</b></p>";
+	}
+
  ?>
 
 </body>
